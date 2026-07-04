aab5b257b270c193f29a62671af92a9061de0b51
Camilo Castro Spadafora <ccspadafora@gmail.com>
2026-07-04 12:27:49 -0500

    correcciones admin django2

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -23,7 +23,6 @@ _use_s3_storage = all([
 
 if _use_s3_storage:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 AWS_DEFAULT_ACL = None
 AWS_S3_FILE_OVERWRITE = False
