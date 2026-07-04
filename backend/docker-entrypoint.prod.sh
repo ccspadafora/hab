1e9886ab172babd22e897c3bdc7a40193d8907cc
Camilo Castro Spadafora <ccspadafora@gmail.com>
2026-07-04 12:33:00 -0500

    correcciones admin django3

--- backend/docker-entrypoint.prod.sh
+++ backend/docker-entrypoint.prod.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 set -e
 
+mkdir -p /app/staticfiles
+chown -R nonroot:nonroot /app/staticfiles
+
+run_as_nonroot() {
+    su -s /bin/sh nonroot -c "$1"
+}
+
 echo "Running migrations..."
-python manage.py migrate --noinput
+run_as_nonroot "python manage.py migrate --noinput"
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+run_as_nonroot "python manage.py collectstatic --noinput"
 
 echo "Starting gunicorn..."
-exec gunicorn config.wsgi:application \
+exec su -s /bin/sh nonroot -c "gunicorn config.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 4 \
     --worker-class gthread \
     --threads 2 \
     --timeout 60 \
     --access-logfile - \
-    --error-logfile -
+    --error-logfile -"
